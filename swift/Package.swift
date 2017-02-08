80ea3609ddd75a713ef0fe2765e580567bca0fde
Tan Jay Jun <tan@jayjun.com>
2017-02-08 23:28:33 +0800

    Use Kitura 1.6.0

--- swift/Package.swift
+++ swift/Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "MyApplication",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 5)
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6)
     ]
 )
